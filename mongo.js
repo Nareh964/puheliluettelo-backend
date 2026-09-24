e30d6396e61d63f301c74666800a6f4cfc4b85b1
Nareh964 <nareh5490@gmai.com>
2026-09-24 17:57:25 +0300

    add validation

--- mongo.js
+++ mongo.js
@@ -29,7 +29,7 @@ if (process.argv.length === 3) {
     mongoose.connection.close()
   })
 } else if (process.argv.length === 5) {
- 
+
   const name = process.argv[3]
   const number = process.argv[4]
 
